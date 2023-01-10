41d663ee184f0c912b78a5c5c3d05dd8c1cc600f
bravenbright <bbryantcs@gmail.com>
2023-01-09 23:51:08 -0600

    added JS functionality to prompt user questions in CLI

--- index.js
+++ index.js
@@ -4,15 +4,33 @@
    4) After new employee information is entered then it will prompt you to do step 2 again
    5) else completes input of commandline and generates html page
    */
-
-
-const {employeeQuest, managerQuest} = require('./lib/prompts');
-const {Employee, Manager, TeamMember} = require('./lib/models'); 
 const fs = require('fs');
 const inquirer = require('inquirer');
 const employeeData = []
 
-console.log(employeeQuest);
+const managerQuest = [
+   {
+      name: 'managerName',
+      message: 'What is the Team Manager Name?',
+      type: 'input',
+   },
+   {
+      name: 'employeeId',
+      message: 'What is the employee ID of the manager?',
+      type: 'input'
+   },
+   {
+      name: 'email',
+      message: 'Enter manager email',
+      type: 'input'
+   },
+   {
+      name: 'officeNum',
+      message: 'What is the manager Office Number?',
+      type: 'input'
+   }
+];
+
 function promptManager() {
    return inquirer.prompt(managerQuest).then(function (answers) {
       employeeData.push(answers)
@@ -20,7 +38,12 @@ function promptManager() {
    })
 }
 
-inquirer.prompt(employeeQuest)
+function promptEmployee() {
+   return inquirer.prompt(employeeQuest).then(function (answers) {
+      employeeData.push(answers)
+      console.log(answers);
+   })
+}
 
 function writeToFile(writeFile, employeeInfo) {
    fs.writeFile(writeFile, emplyeeInfo, (error) => {
@@ -28,20 +51,63 @@ function writeToFile(writeFile, employeeInfo) {
    })
 }
 
-
-
 function init() {
    promptManager()
       .then(data => {
-         console.log('Generating HTML...');
-         writeToFile('index.html', generateMarkdown({ ...data }));
+         console.log('Generating Team Manager...');
+         // writeToFile('index.html', generateRoster({ ...data }));
+         promptEmployee()
+         .then(data => {
+            console.log('Generating Team Member')
+         })
       })
       .catch(function (error) {
          console.error(error);
 
       });
-
 };
 
+init();
+
+const employeeQuest = [  
+   {
+      name: 'openingPrompt',
+      message: 'What type of Team Member would you like to create on your company Roster?',
+      type: 'list',
+      choices:['Engineer', 'Intern', 'Exit']
+   },
+   {
+      name: 'name',
+      message: 'What is employee name?',
+      type: 'input'
+   },
+   { 
+      name: 'employeeId',
+      message: 'Enter employee ID here.',
+      type: 'input'
+   },
+   {
+      name: 'email',
+      message: 'Enter employee email',
+      type: 'input'
+   },
+   {
+      name: 'github',
+      message: 'Enter employee github username.',
+      type: 'input'
+   }
+];
+
+// inquirer.prompt(employeeQuest)
+
+
+
+
+// function writeToFile(writeFile, employeeInfo) {
+//    fs.writeFile(writeFile, emplyeeInfo, (error) => {
+//       error ? console.error(error) : console.log('New team member is created.')
+//    })
+// }
+
 //  <a href = mailto
-// init();
+
